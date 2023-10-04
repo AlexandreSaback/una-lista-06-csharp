444f3114a7e2b1edb37a6fe0bbc6e6e8585a8825
AlexandreSaback <78316698+AlexandreSaback@users.noreply.github.com>
2023-10-04 20:43:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Objects
+{
+    static void Main()
+    {
+       // Vetor
+       int[] vetor = new int[] { 1, 2, 3, 4, 5 };
+
+       // Matriz
+       int[,] matriz = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+      
+       // Cubo
+       int[,,] cubo = new int[,,] { { { 1, 2, 3 }, { 4, 5, 6 } }, { { 7, 8, 9 }, { 10, 11, 12 } } };
+
+    }
+
+}
